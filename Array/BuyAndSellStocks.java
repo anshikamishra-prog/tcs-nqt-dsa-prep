731e8a0bb9ea1712f32729a859477485cd0e230e
anshikamishra-prog <anshikamishra4002@gmail.com>
2026-03-10 01:28:44 +0530

    Added Buy & Sell Stocks

---
+++ Array/BuyAndSellStocks.java
@@ -0,0 +1,22 @@
+package Array;
+public class BuyAndSellStocks{
+    public static int maximumProfit(int prices[]) {
+        int minPrice = Integer.MAX_VALUE;   
+        int maxProfit = 0 ;
+
+        for(int i = 0 ; i<prices.length ; i++) {
+            if(prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+            int profit = prices[i] - minPrice;
+            if(profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String args[]) {
+        int prices[] = {7,1,5,3,6};
+        System.out.println(maximumProfit(prices));
+    }
+}
--- Array/RotateArrayBykElements.java
+++ Array/RotateArrayBykElements.java
@@ -1,5 +1,4 @@
 package Array;
-
 import java.util.Arrays;
 
 public class RotateArrayBykElements {
